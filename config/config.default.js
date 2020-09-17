ef427bce7833a0b1403d55a4be434ded7bd95c39
hackycy <qa894178522@qq.com>
2020-09-17 22:28:47 +0800

    fix：update config.default.js

--- config/config.default.js
+++ config/config.default.js
@@ -6,20 +6,36 @@
  * @property {String} SOME_KEY - some description
  */
 exports.typeorm = {
-  type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: '123456',
-  database: 'siyee-api',
-  synchronize: true,
-  logging: false,
-  entities: [ 'app/entity/**/*.ts' ],
-  migrations: [ 'app/migration/**/*.ts' ],
-  subscribers: [ 'app/subscriber/**/*.ts' ],
-  cli: {
-    entitiesDir: 'app/entity',
-    migrationsDir: 'app/migration',
-    subscribersDir: 'app/subscriber',
-  },
-};
+  client: {
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'test',
+    password: '123456',
+    database: 'test',
+    synchronize: true,
+    logging: false
+  }
+}
+
+// config.typeorm = {
+//   clients: [{
+//     name: "model1",
+//     type: "mysql",
+//     host: "localhost",
+//     port: 3306,
+//     username: "root",
+//     password: "admin",
+//     database: "db1",
+//     synchronize: true
+//   }, {
+//     name: "model2",
+//     type: "mysql",
+//     host: "localhost",
+//     port: 3306,
+//     username: "root",
+//     password: "admin",
+//     database: "db2",
+//     synchronize: true
+//   }]
+// }
