7c3b3baf51f7b7fcbb7025e357282903c15c18d0
hackycy <qa894178522@qq.com>
2020-09-20 00:50:34 +0800

    refactor：entitiesDir配置方式变更

--- index.d.ts
+++ index.d.ts
@@ -2,25 +2,30 @@ import { ConnectionOptions, Connection } from 'typeorm'
 
 declare module 'egg' {
 
-  interface PluginConnectionOptions extends ConnectionOptions {
-    entitiesDir?: string
+  interface Context {
+    connection: Connection
+    getConnection(connectionName?: string): Connection
   }
 
-  // interface Context {
-  //   connection: Connection
-  //   getConnection(connectionName?: string): Connection
-  // }
+  interface EntityDirOptions {
+    entitiesDir?: string,
+    name?: string
+  }
 
   interface EggAppConfig {
     typeorm: {
       /**
        * @description typeorm conn option
        */
-      client?: PluginConnectionOptions;
+      client?: ConnectionOptions
       /**
        * @description typeorm conns option
        */
-      clients?: Array<PluginConnectionOptions>;
+      clients?: Array<ConnectionOptions>
+      /**
+       * @description typeorm entity dir
+       */
+      entities?: Array<EntityDirOptions>
     },
   }
 }
