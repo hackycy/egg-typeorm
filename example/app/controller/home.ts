7503f32e003ac446319467bc5b718965518b1575
hackycy <qa894178522@qq.com>
2020-09-22 12:16:46 +0800

    test：update example

--- example/app/controller/home.ts
+++ example/app/controller/home.ts
@@ -3,6 +3,6 @@ import { Controller } from 'egg';
 export default class HomeController extends Controller {
   public async index() {
     const { ctx } = this;
-    ctx.body = await this.ctx.repo.sys.User.find();
+    ctx.body = await this.ctx.repo.db2.User.find();
   }
 }
--- example/typings/typeorm.d.ts
+++ example/typings/typeorm.d.ts
@@ -5,16 +5,17 @@ import 'egg';
 import { TreeRepository, Repository } from 'typeorm'
 import Db1User from '../app/entity/db1/user'
 import Db2User from '../app/entity/db2/user'
+import Db2SysUser from '../app/entity/db2/sys/user'
 
 declare module 'egg' {
   interface Context {
     entity: {
       User: typeof Db1User
-      db2: { User: typeof Db2User }
+      db2: { User: typeof Db2User; sys: { User: typeof Db2SysUser } }
     }
     repo: {
       User: Repository<Db1User>
-      db2: { User: Repository<Db2User> }
+      db2: { User: Repository<Db2User>; sys: { User: Repository<Db2SysUser> } }
     }
   }
 }
