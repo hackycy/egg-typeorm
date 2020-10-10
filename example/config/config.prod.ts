243471724837926079d84e3e5f92d3dc58ded206
hackycy <qa894178522@qq.com>
2020-10-10 17:08:05 +0800

    docs：测试生产环境运行效果

--- example/config/config.prod.ts
+++ example/config/config.prod.ts
@@ -2,5 +2,44 @@ import { EggAppConfig, PowerPartial } from 'egg';
 
 export default () => {
   const config: PowerPartial<EggAppConfig> = {};
+
+  // typeorm
+  config.typeorm = {
+    clients: [
+      {
+        name: 'default',
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: 'root',
+        password: '123456',
+        database: 'test',
+        synchronize: true,
+        logging: 'all',
+      },
+      {
+        name: 'db2',
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: 'root',
+        password: '123456',
+        database: 'test2',
+        synchronize: true,
+        logging: 'all',
+      },
+      {
+        name: 'db3',
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: 'root',
+        password: '123456',
+        database: 'test3',
+        synchronize: true,
+        logging: 'all',
+      }
+    ]
+  }
   return config;
 };
