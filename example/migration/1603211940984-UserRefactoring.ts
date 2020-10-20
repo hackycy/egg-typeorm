6876616e706c51220db56c5eb2f700d50e7fe680
hackycy <qa894178522@qq.com>
2020-10-21 00:51:20 +0800

    test：测试迁移

---
+++ example/migration/1603211940984-UserRefactoring.ts
@@ -0,0 +1,13 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class UserRefactoring1603211940984 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('ALTER TABLE user CHANGE name name_migra varchar(255)');
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('ALTER TABLE user CHANGE name_migra name varchar(255)'); // 恢复"up"方法所做的事情
+  }
+
+}
