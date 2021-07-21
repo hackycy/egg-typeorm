31e77c9524ff63cccb2bb326c1b486e9494dc7ea
hackycy <qa894178522@qq.com>
2021-07-21 10:37:46 +0800

    docs：update example

--- example/app/controller/home.ts
+++ example/app/controller/home.ts
@@ -4,6 +4,6 @@ export default class HomeController extends Controller {
   public async index() {
     const { ctx } = this;
     this.app.logger.info(mark, 'this is logger info');
-    ctx.body = await this.ctx.repo.db2.User.find({ id: 0 });
+    ctx.body = await this.service.test.findById(1);
   }
 }
